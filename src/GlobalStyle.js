66df5ad28a577778416128e46b3ddf08015a652b
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2023-11-26 16:35:32 +0400

    added hero, thumb and grid with styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     :root {
         --maxWidth: 1280px;
-        --white: #000;
+        --white: #fff;
         --lightGrey: #eee;
         --medGrey: #353535;
         --darkGrey: #1c1c1c;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config"
+import React, { useState, useEffect } from "react";
+//config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+//Components
+import HeroImage from "./HeroImage";
+import Grid from "./grid";
+import Thumb from "./Thumb";
+//Hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
+//image
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-
+    const { state, loading, error } = useHomeFetch()
     return (
-        <div></div>
+        <>
+            {state.results[0] ?
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                />
+                : null
+            }
+            <Grid
+                header="Popular Movies">
+                {state.results.map((movie) => (
+                    <Thumb key={movie.id} clickable={true}
+                        image={movie.poster_path ?
+                            IMAGE_BASE_URL + POSTER_SIZE +
+                            movie.poster_path : NoImage
+                        }
+                        movieId={movie.id}
+                    />
+                ))}
+            </Grid>
+
+
+        </>
+
     )
 }
 
