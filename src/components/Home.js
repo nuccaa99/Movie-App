76b183b878ef3c5aab068b8fead86d83e71032e0
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2023-11-27 14:00:03 +0400

    added spinner component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 //config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 //Components
 import HeroImage from "./HeroImage";
 import Grid from "./grid";
 import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 //Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
 
@@ -35,7 +36,7 @@ const Home = () => {
                     />
                 ))}
             </Grid>
-
+            <Spinner />
 
         </>
 
